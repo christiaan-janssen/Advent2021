a37e1827bd304fc916c7b8f39f99379c445d6242
christiaan-janssen <christiaanjanssen@outlook.com>
2021-12-01 08:05:33 +0100

    Day one done

--- day01.py
+++ day01.py
@@ -2,15 +2,33 @@
 
 from utils.tools import get_input
 
+
 def puzzle_01():
-    data = get_input("day01.input", " ")
-    print(data)
+    """Calculate the sonar depth increase."""
+    data = get_input("day01.input", "\n")
+    data = [int(val) for val in data[:-1]]
+    count = 0
+    for i, var in enumerate(data):
+        if not i+1 > len(data)-1:
+            if var < data[i+1]:
+                count += 1
+
+    return count
+
 
 def puzzle_02():
-    data = get_input("day01.input", " ")
-    print(data)
+    input = get_input("day01.input", "\n")
+    count = 0
+    data = [int(val) for val in input[:-1]]
+    for i, var in enumerate(data):
+        if not i+3 > len(data)-1:
+            a = sum(data[i:i+3])
+            b = sum(data[i+1:i+4])
+            if a < b:
+                count += 1
+
+    return count
 
 if __name__ == "__main__":
-    puzzle_01()
-    puzzle_02()
-    
+    print(f"Day 01 a: {puzzle_01()}")
+    print(f"Day 01 b: {puzzle_02()}")
