78f6952fd69eacd0682d7460a3e0524554a63aff
christiaan-janssen <christiaanjanssen@outlook.com>
2021-12-03 11:26:57 +0100

    Puzzle b complete, day 3 done.

--- day03.py
+++ day03.py
@@ -1,5 +1,6 @@
 """Advent of Code 2021 Day 03."""
 
+from typing import List
 from utils.tools import get_input
 
 
@@ -17,23 +18,74 @@ test_data = """00100
 01010
 """.split("\n")
 
-def count_most_common(line):
+
+def count_most_common(line: List[str]) -> int:
+    """Calculate the most common bit in a 'binary number'.
+    :param line A list with the characters of the 'binary number'.
+    :return: the most commen bit in the number.
+    """
     zero, one = 0, 0
     for c in line:
         if int(c) == 0:
-            zero += 1 
+            zero += 1
         elif int(c) == 1:
             one += 1
     return 0 if zero > one else 1
 
-def count_leaste_common(line):
+
+def count_leaste_common(line: List[str]) -> int:
+    """Calculate the leaste common bit in a 'binary number'.
+    :param line A list with the characters of the 'binary number'.
+    :return: the most commen bit in the number.
+    """
     zero, one = 0, 0
     for c in line:
         if int(c) == 0:
-            zero += 1 
+            zero += 1
         elif int(c) == 1:
             one += 1
     return 0 if zero < one else 1
+
+
+def calculate_oxygen_generator_rating(data):
+    pos = 0
+    while len(data) > 1:
+        byte = '1'
+        one = list(filter(lambda x: x[pos] == byte, data))
+
+        byte = '0'
+        zero = list(filter(lambda x: x[pos] == byte, data))
+
+        if len(one) == len(zero):
+            data = one
+        elif len(one) > len(zero):
+            data = one
+        else:
+            data = zero
+        pos += 1
+
+    return data[0]
+
+
+def calculate_CO2_scrubber_rating(data):
+    pos = 0
+    while len(data) > 1:
+        byte = '1'
+        one = list(filter(lambda x: x[pos] == byte, data))
+
+        byte = '0'
+        zero = list(filter(lambda x: x[pos] == byte, data))
+
+        if len(one) == len(zero):
+            data = zero
+        elif len(one) > len(zero):
+            data = zero
+        else:
+            data = one
+        pos += 1
+    print(data)
+    return data[0]
+
 
 def puzzle_01():
     data = get_input("day3.input", "\n")
@@ -54,10 +106,14 @@ def puzzle_01():
 
 
 def puzzle_02():
-    data = get_input("day03.input", " ")
-    print(data)
+    data = get_input("day3.input", "\n")[:-1]
+
+    oxygen = calculate_oxygen_generator_rating(data)
+    co2 = calculate_CO2_scrubber_rating(data)
+
+    return int(oxygen, 2) * int(co2, 2)
+
 
 if __name__ == "__main__":
     print(f"Day 03 a: {puzzle_01()}")
-    #print(f"Day 03 b: {puzzle_02()}")
-    
+    print(f"Day 03 b: {puzzle_02()}")
