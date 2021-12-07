d0f26771200e2364bed30a4d1052ec8b4f928339
christiaan-janssen <christiaanjanssen@outlook.com>
2021-12-07 11:38:04 +0100

    Puzzle b done, day 7 done.

--- day07.py
+++ day07.py
@@ -2,6 +2,7 @@
 
 from typing import List
 from utils.tools import get_input
+from timeit import default_timer as timer
 
 test_input = """16,1,2,0,4,2,7,1,2,14""".split()
 
@@ -10,14 +11,37 @@ def move_to(crab: int, position: int) -> int:
     """Return the cost to move a crab to a position."""
     return abs(position - crab)
 
-def move_more_fuel(crab: int, position:int) -> int:
-    pass
+
+def move_to_2(crab: int, position: int) -> int:
+    """Return the cost to move a crab to a position, using more fuel."""
+    num = abs(position - crab)
+    fuel = 0
+    while (num > 0):
+        fuel += num
+        num -= 1
+
+    return fuel
+
 
 def align_crabs(crab_positions: List[int]):
-    """Align the crab positions, using as less feul as possble."""
+    """Align the crab positions, using as less fuel as possble."""
     least_fuel_cost = None
     for position in range(crab_positions[-1]):
         fuel_cost = sum([move_to(crab, position) for crab in crab_positions])
+        if least_fuel_cost is None:
+            least_fuel_cost = fuel_cost
+        elif fuel_cost < least_fuel_cost:
+            least_fuel_cost = fuel_cost
+
+    return least_fuel_cost
+
+
+def align_crabs_b(crab_positions: List[int]):
+    """Align the crab positions, using as less fuel as possble."""
+    # TODO this is waaaay to slow, 1m25.
+    least_fuel_cost = None
+    for position in range(crab_positions[-1]):
+        fuel_cost = sum([move_to_2(crab, position) for crab in crab_positions])
         if least_fuel_cost is None:
             least_fuel_cost = fuel_cost
         elif fuel_cost < least_fuel_cost:
@@ -36,10 +60,14 @@ def puzzle_01():
 
 
 def puzzle_02():
-    data = get_input("day07.input", " ")
-    print(data)
+    """Puzzle 02 day 7."""
+    data = get_input("day7.input", " ")
+    # data = test_input
+    data = [int(i) for i in data[0].split(',')]
+    data.sort()
+    return align_crabs_b(data)
 
 
 if __name__ == "__main__":
     print(f"Day 07 a: {puzzle_01()}")
-    # puzzle_02()
+    print(f"Day 07 b: {puzzle_02()}")
--- tests/test_day07.py
+++ tests/test_day07.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-from day07 import align_crabs, move_to
+from day07 import align_crabs, align_crabs_b, move_to, move_to_2
 
 class TestAlignCrabs(unittest.TestCase):
     def test_align(self):
@@ -10,3 +10,10 @@ class TestAlignCrabs(unittest.TestCase):
 
     def test_move_a_crab(self):
         self.assertEqual(move_to(16, 2), 14)
+
+    def test_move_a_crab_2(self):
+        self.assertEqual(move_to_2(16, 5), 66)
+
+    def test_aling_b(self):
+        test_input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+        self.assertEqual(align_crabs_b(test_input), 168)
